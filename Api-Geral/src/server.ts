f2c9f780095ee0a717782c6f0bebc4edcdb0d5dd
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-06-29 17:20:14 -0300

    Save

--- Api-Geral/src/server.ts
+++ Api-Geral/src/server.ts
@@ -1,36 +1,44 @@
-import express,{ErrorRequestHandler} from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
-import cors from 'cors'
-import router from './routes'
+import express, { ErrorRequestHandler } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+import router from "./routes";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
-import useragent from 'express-useragent'
-dotenv.config()
+import useragent from "express-useragent";
+dotenv.config();
 
 const app = express();
-app.use(useragent.express())
-const httpServer = createServer(app)
-const io = new Server(httpServer,{ cors: {
-  origin: "*"
-}})
-app.use(cors())
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use(express.static(path.join(__dirname,'../public')))
-app.use(router)
-io.on("connection", (socket) => {
-    socket.on('Retorno',(data)=>{
-        socket.broadcast.emit('status',data)
-    })
+app.use(useragent.express());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
 });
-const ErrorHandle:ErrorRequestHandler = (error,req,res,next) =>{
-    if(error){
-        res.json({error:'Error, tente novamente mais tarde!',err:error})
-    }
-}
-app.use(ErrorHandle)
-app.use((req,res)=>{
-    res.json({error:'Acesso negado!'})
-})
-httpServer.listen(process.env.PORT)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(router);
+io.on("connection", (socket) => {
+  socket.on("Retorno", (data) => {
+    socket.broadcast.emit("status", data);
+  });
+});
+const ErrorHandle: ErrorRequestHandler = (error, req, res, next) => {
+  if (error) {
+    res.json({ error: "Error, tente novamente mais tarde!", err: error });
+  }
+};
+app.use(ErrorHandle);
+app.use((req, res) => {
+  res.json({ error: "Acesso negado!" });
+});
+
+app.get("/teste", (req, res) =>
+  res.json({ message: "Welcome to this new API!" })
+);
+
+// httpServer.listen(process.env.PORT)
+app.listen(process.env.PORT);
--- Api-Integration/src/server.ts
+++ Api-Integration/src/server.ts
@@ -1,25 +1,30 @@
-import express,{ErrorRequestHandler} from 'express'
-import path from 'path'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import routes from './routes'
-dotenv.config()
-const app = express()
+import express, { ErrorRequestHandler } from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
+import routes from "./routes";
+dotenv.config();
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,'../public')))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(routes)
+app.use(routes);
 
-const ErrorHandle:ErrorRequestHandler = (error,req,res,next) =>{
-    if(error){
-        res.json({error:'Acesso negado!'})
-    }
-} 
-app.use(ErrorHandle)
-app.use((req,res)=>{
-    res.json({error:'Acesso negado!'})
-})
-app.listen(process.env.PORT)
+const ErrorHandle: ErrorRequestHandler = (error, req, res, next) => {
+  if (error) {
+    res.json({ error: "Acesso negado!" });
+  }
+};
+app.use(ErrorHandle);
+app.use((req, res) => {
+  res.json({ error: "Acesso negado!" });
+});
+
+app.get("/teste", (req, res) =>
+  res.json({ message: "Welcome to this new API!" })
+);
+
+app.listen(process.env.PORT);
