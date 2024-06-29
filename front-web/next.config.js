b1a704935a2e7c9197720232b0cd974bd87a615c
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-06-29 13:17:53 -0300

    Save

--- front-web/next.config.js
+++ front-web/next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-const withFonts = require("next-fonts");
-const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["localhost", "api.wolfypay.com", "192.168.200.23"],
   },
-  staticPageGenerationTimeout: 300,
-  swcMinify: true,
+  // staticPageGenerationTimeout: 300,
+  // swcMinify: true,
 };
 
-module.exports = withPlugins([withFonts], nextConfig);
+module.exports = nextConfig;
