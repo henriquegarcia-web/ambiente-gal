27e285edf2ff287d68beb2328663f4ef79ba56b2
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-06-30 12:04:43 -0300

    Save

--- front-web/src/api/apiProdutor.js
+++ front-web/src/api/apiProdutor.js
@@ -1,40 +1,43 @@
-import axios from 'axios'
-let obj
-if (typeof window !== 'undefined') {
-    const getAuthUser = localStorage.getItem('authUser')
-    obj = getAuthUser=='undefined' ||getAuthUser==null?'': JSON.parse(getAuthUser?getAuthUser:'')
-  }
+import axios from "axios";
+let obj;
+if (typeof window !== "undefined") {
+  const getAuthUser = localStorage.getItem("authUser");
+  obj =
+    getAuthUser == "undefined" || getAuthUser == null
+      ? ""
+      : JSON.parse(getAuthUser ? getAuthUser : "");
+}
 
 const axiosInstance = axios.create({
-    baseURL:`${process.env.NEXT_PUBLIC_API_URL}/usuario`,
-    headers:{
-    'Authorization':`Bearer ${obj?.token}`,
-    'Content-Type':'application/json'
-    }
-})
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/produtor`,
+  headers: {
+    Authorization: `Bearer ${obj?.token}`,
+    "Content-Type": "application/json",
+  },
+});
 export const Api = {
-    Listagem:async(dados)=>{
-        let response = await axiosInstance.get('/',dados)
-        return response.data
-    },
-    ListagemIndividual:async(dados)=>{
-        let response = await axiosInstance.get(`/${dados.id}`)
-        return response.data
-    },
-    Cadastrar:async(dados)=>{
-        let response = await axiosInstance.post('/',dados)
-        return response.data
-    },
-    Editar:async(dados)=>{
-        let response = await axiosInstance.put('/',dados)
-        return response.data
-    },
-    Taxas:async(dados)=>{
-        let response = await axiosInstance.put('/taxas',dados)
-        return response.data
-    },
-    Excluir:async(dados)=>{
-        let response = await axiosInstance.delete(`/${dados.id}`)
-        return response.data
-    }
-}
+  Listagem: async (dados) => {
+    let response = await axiosInstance.get("/", dados);
+    return response.data;
+  },
+  ListagemIndividual: async (dados) => {
+    let response = await axiosInstance.get(`/${dados.id}`);
+    return response.data;
+  },
+  Cadastrar: async (dados) => {
+    let response = await axiosInstance.post("/", dados);
+    return response.data;
+  },
+  Editar: async (dados) => {
+    let response = await axiosInstance.put("/", dados);
+    return response.data;
+  },
+  Taxas: async (dados) => {
+    let response = await axiosInstance.put("/taxas", dados);
+    return response.data;
+  },
+  Excluir: async (dados) => {
+    let response = await axiosInstance.delete(`/${dados.id}`);
+    return response.data;
+  },
+};
